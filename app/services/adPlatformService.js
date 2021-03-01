52cbb17048f70d50b3a658f81badc56048f633be
John Kim <jkim@coolerscreens.com>
2021-03-01 19:01:50 +0000

    Merged PR 4982: handle AdType coolerTakeover for downloading ad assets
    handle coolerTakeover for downloading ad assets
    
    Related work items: #7884, #7972

--- app/services/adPlatformService.js
+++ app/services/adPlatformService.js
@@ -31,15 +31,15 @@ const _tagDirectoryPath = path.join(config.storageLocalAdPlatformDataDir, _tagAs
 const _labelDirectoryPath = path.join(config.storageLocalAdPlatformDataDir, _labelAssetTypeKDirectory);
 const _spotlightAdDirectoryPath = path.join(config.storageLocalAdPlatformDataDir, _spotlightAdAssetTypeDirectory);
 const _assetTypeToLocalDirectory = {
-    [_fullDoorAdAssetTypeKey]: _fullDoorAdDirectoryPath,
-    [_middleBannerAssetTypeKey]: _middleBannerDirectoryPath,
-    [_topBannerAssetTypeKey]: _topBannerDirectoryPath,
-    [_nativeAdAssetTypeKey]: _nativeAdDirectoryPath,
-    [_nativeFilterAssetTypeKey]: _nativeFilterDirectoryPath,
-    [_tagAssetTypeKey]: _tagDirectoryPath,
-    [_labelAssetTypeKey]: _labelDirectoryPath,
-    [_spotlightAdAssetTypeKey]: _spotlightAdDirectoryPath
-}
+  [_fullDoorAdAssetTypeKey]: _fullDoorAdDirectoryPath,
+  [_middleBannerAssetTypeKey]: _middleBannerDirectoryPath,
+  [_topBannerAssetTypeKey]: _topBannerDirectoryPath,
+  [_nativeAdAssetTypeKey]: _nativeAdDirectoryPath,
+  [_nativeFilterAssetTypeKey]: _nativeFilterDirectoryPath,
+  [_tagAssetTypeKey]: _tagDirectoryPath,
+  [_labelAssetTypeKey]: _labelDirectoryPath,
+  [_spotlightAdAssetTypeKey]: _spotlightAdDirectoryPath,
+};
 const _adPlatformNothingChanged = 'Not modified';
 
 let _firstAdPlatformServiceRun = true;
@@ -47,147 +47,184 @@ let _firstAdPlatformServiceRun = true;
 exports.adPlatformNothingChanged = _adPlatformNothingChanged;
 
 exports.downloadAndSaveAdPlatformAssets = async function (adPlatformData, forceDownload = true) {
-    if (!adPlatformData || utils.isEmptyArray(adPlatformData)) {
-        logger.info('Empty adPlatform data, nothing to download. Aborting.');
-        return;
+  if (!adPlatformData || utils.isEmptyArray(adPlatformData)) {
+    logger.info('Empty adPlatform data, nothing to download. Aborting.');
+    return;
+  }
+  if (adPlatformData === _adPlatformNothingChanged) {
+    logger.info('No changes in ad-platform data');
+    return;
+  }
+  for (const campaign of adPlatformData) {
+    if (!campaign) {
+      logger.warn(`An empty campaign encountered`);
+      return;
     }
-    if (adPlatformData === _adPlatformNothingChanged) {
-        logger.info('No changes in ad-platform data');
-        return;
+    if (!campaign.AdType) {
+      logger.error(`Cannot interpret adType for campaign ${JSON.stringify(campaign)}`);
+      return;
     }
-    for (const campaign of adPlatformData) {
-        if (!campaign) {
-            logger.warn(`An empty campaign encountered`);
-            return;
-        }
-        if (!campaign.AdType) {
-            logger.error(`Cannot interpret adType for campaign ${JSON.stringify(campaign)}`);
-            return;
-        }
-        if (campaign.AdProvider !== 'CSI') {
-            logger.info(`Non CSI campaign. No assets to download, hence skipping. CampaignID: ${campaign.CampaignId}, AdType: ${campaign.AdType}`);
-            continue;
-        }
-        if (!campaign.Assets) {
-            logger.warn(`Campaign has no assets: ${JSON.stringify(campaign)}`);
-        }
+    if (campaign.AdProvider !== 'CSI') {
+      logger.info(`Non CSI campaign. No assets to download, hence skipping. CampaignID: ${campaign.CampaignId}, AdType: ${campaign.AdType}`);
+      continue;
+    }
+    if (!campaign.Assets) {
+      logger.warn(`Campaign has no assets: ${JSON.stringify(campaign)}`);
+    }
+    
+    
+    createDirectoriesForAssets();
+    
+    // In the case of a coolerTakeover, the adType must be grabbed at the asset level instead of at the campaign level
+    if (campaign.AdType === 'CoolerTakeover') {
+      await saveCoolerTakeoverAdAssets(campaign, forceDownload);
+    } else {
+      await saveAdAssets(campaign, forceDownload);
+    }
+  }
+};
 
-        const dirToSaveTo = _assetTypeToLocalDirectory[campaign.AdType]
-        if (!dirToSaveTo) {
-            logger.error(`Error: Unfamiliar AdType encountered: ${campaign.AdType}. Skipping.`);
-            return;
-        }
-        const assetsToSave = utils.toDistinctDictionary(campaign.Assets.filter(asset => asset.SasLink && asset.FileName),
-            asset => asset.FileName, asset => asset.SasLink);
 
-        createDirectoriesForAssets();
-        for (var assetFilename in assetsToSave) {
-            if (assetsToSave.hasOwnProperty(assetFilename)) {
-                const shouldRedownloadFile = fileRecoveryUtils.shouldRedownloadFile(dirToSaveTo, assetFilename);
-                if (forceDownload || shouldRedownloadFile) {
-                    const assetUrl = assetsToSave[assetFilename];
-                    if (!forceDownload) {
-                        logger.info(`Noticed an ad-platform asset that was not downloaded in previous cycle: ${assetFilename}, will download again`);
-                    }
+const saveAdAssets = async (campaign, forceDownload) => {
+  const assetsToSave = utils.toDistinctDictionary(campaign.Assets.filter(asset => asset.SasLink && asset.FileName),
+    asset => asset.FileName, asset => asset.SasLink);
+  const dirToSaveTo = _assetTypeToLocalDirectory[campaign.AdType];
+  if (!dirToSaveTo) {
+    logger.error(`Error: Unfamiliar AdType encountered in ${campaign.AdType} campaign ${campaign.CampaignId}: ${campaign.AdType}. Skipping.`);
+    return;
+  }
+  
+  for (let assetFilename in assetsToSave) {
+    if (assetsToSave.hasOwnProperty(assetFilename)) {
+      const shouldRedownloadFile = fileRecoveryUtils.shouldRedownloadFile(dirToSaveTo, assetFilename);
+      const assetUrl = assetsToSave[assetFilename];
+      if (forceDownload || shouldRedownloadFile) {
+        if (!forceDownload) {
+          logger.info(`Found an ad-platform asset that was not downloaded in previous cycle: ${assetFilename}, will download again`);
+        }
         
-                    const shouldUseIfModifiedSince = !shouldRedownloadFile;
-                    await httpService.downloadAndSaveAsset(assetUrl, assetFilename, dirToSaveTo, shouldUseIfModifiedSince);
-                }
-            }
+        const shouldUseIfModifiedSince = !shouldRedownloadFile;
+        await httpService.downloadAndSaveAsset(assetUrl, assetFilename, dirToSaveTo, shouldUseIfModifiedSince);
+      }
+    }
+  }
+};
+
+const saveCoolerTakeoverAdAssets = async (campaign, forceDownload) => {
+  const assetsToSave = utils.toDistinctDictionary(campaign.Assets.filter(asset => asset.SasLink && asset.FileName),
+    asset => asset.FileName, asset => asset);
+  for (let assetFilename in assetsToSave) {
+    if (assetsToSave.hasOwnProperty(assetFilename)) {
+      const asset = assetsToSave[assetFilename];
+      const dirToSaveTo = _assetTypeToLocalDirectory[asset.AdType];
+      if (!dirToSaveTo) {
+        logger.error(`Error: Unfamiliar AdType encountered in ${campaign.AdType} campaign ${campaign.CampaignId}: ${campaign.AdType}. Skipping.`);
+        return;
+      }
+      const shouldRedownloadFile = fileRecoveryUtils.shouldRedownloadFile(dirToSaveTo, assetFilename);
+      if (forceDownload || shouldRedownloadFile) {
+        const assetUrl = asset.SasLink;
+        if (!forceDownload) {
+          logger.info(`Found an ad-platform asset that was not downloaded in previous cycle: ${assetFilename}, will download again`);
         }
-    };
-}
+        
+        const shouldUseIfModifiedSince = !shouldRedownloadFile;
+        await httpService.downloadAndSaveAsset(assetUrl, assetFilename, dirToSaveTo, shouldUseIfModifiedSince);
+      }
+    }
+  }
+};
 
 exports.getAdPlatformData = async function (forceDownload = false) {
-let adPlatformUrl;
-
-    try {
-        const adPlatformDataLastModified = utils.getFileLastModifiedTime(
-            path.join(_storageLocalAdPlatformDataDir, _adPlatformDataFilename));
-        const getHeaders = forceDownload ? {} : getAdPlatformRequestHeaders(adPlatformDataLastModified);
-        adPlatformUrl = await buildAdPlatformGetUrl();
-        
-        const adPlatformDataResponse = await axios.get(adPlatformUrl, {
-            headers: getHeaders,
-        });
-        let adPlatformData = adPlatformDataResponse.data;
-
-        if (!utils.isArray(adPlatformData)) {
-            // TODO: better handling
-            logger.warn(`adPlatformData is not an array. Will fallback to read from disk. Contents: ${adPlatformData}`);
-            adPlatformData = await readAdPlatformDataFromDisk();
-        } else {
-            saveAdPlatformJson(adPlatformData);
-        }
-
-        return adPlatformData;
-    } catch (error) {
-        if (error && error.response) { // HTTP error
-            if (error.response.status === 304) {
-                // Not an error:
-                logger.info(`File at ${utils.toUnconfidentialUrl(adPlatformUrl)} was not modified compared to local copy, will not download.`);
-
-                return _adPlatformNothingChanged;
-            }
-        } else {
-            const stack = error.stack ? error.stack.split("\n") : '';
-            logger.error(`Error getting AdPlatform data from ${adPlatformUrl}: ${error}. [${stack}]`);
-        }
+  let adPlatformUrl;
+  
+  try {
+    const adPlatformDataLastModified = utils.getFileLastModifiedTime(
+      path.join(_storageLocalAdPlatformDataDir, _adPlatformDataFilename));
+    const getHeaders = forceDownload ? {} : getAdPlatformRequestHeaders(adPlatformDataLastModified);
+    adPlatformUrl = await buildAdPlatformGetUrl();
+    
+    const adPlatformDataResponse = await axios.get(adPlatformUrl, {
+      headers: getHeaders,
+    });
+    let adPlatformData = adPlatformDataResponse.data;
+    
+    if (!utils.isArray(adPlatformData)) {
+      // TODO: better handling
+      logger.warn(`adPlatformData is not an array. Will fallback to read from disk. Contents: ${adPlatformData}`);
+      adPlatformData = await readAdPlatformDataFromDisk();
+    } else {
+      saveAdPlatformJson(adPlatformData);
     }
-}
+    
+    return adPlatformData;
+  } catch (error) {
+    if (error && error.response) { // HTTP error
+      if (error.response.status === 304) {
+        // Not an error:
+        logger.info(`File at ${utils.toUnconfidentialUrl(adPlatformUrl)} was not modified compared to local copy, will not download.`);
+        
+        return _adPlatformNothingChanged;
+      }
+    } else {
+      const stack = error.stack ? error.stack.split("\n") : '';
+      logger.error(`Error getting AdPlatform data from ${adPlatformUrl}: ${error}. [${stack}]`);
+    }
+  }
+};
 
 exports.readAdPlatformDataFromDiskSync = function () {
-    let fileFullPath = path.join(_storageLocalAdPlatformDataDir, _adPlatformDataFilename);
-
-    return JSON.parse(utils.readTextFile(fileFullPath));
-}
+  let fileFullPath = path.join(_storageLocalAdPlatformDataDir, _adPlatformDataFilename);
+  
+  return JSON.parse(utils.readTextFile(fileFullPath));
+};
 
 const readAdPlatformDataFromDisk = async function () {
-    let fileFullPath = path.join(_storageLocalAdPlatformDataDir, _adPlatformDataFilename);
-    fs.readFile(fileFullPath, 'utf8', (err, data) => {
-        if (err) {
-            logger.error(`Could not read file from ${fileFullPath}. Details:${err}`);
-            return '';
-        }
+  let fileFullPath = path.join(_storageLocalAdPlatformDataDir, _adPlatformDataFilename);
+  fs.readFile(fileFullPath, 'utf8', (err, data) => {
+    if (err) {
+      logger.error(`Could not read file from ${fileFullPath}. Details:${err}`);
+      return '';
+    }
+    
+    return JSON.parse(data);
+  });
+};
 
-        return JSON.parse(data);
+const getAdPlatformRequestHeaders = function (adPlatformDataLastModified) {
+  // Motive: Since we could have potentially switched doors, the "last modified time" for our
+  // local ad-platform-data file might be related to a different door, and we do not want to
+  // get an empty response based on that. Therfeore, after boot, we will always get adPlatformData.
+  const adPlatformGetHeaders = _firstAdPlatformServiceRun ? {} : {
+    'If-Modified-Since': adPlatformDataLastModified,
+  };
+  
+  _firstAdPlatformServiceRun = false;
+  
+  return adPlatformGetHeaders;
+};
+
+const saveAdPlatformJson = function (adPlatformData) {
+  let fileFullPath = path.join(_storageLocalAdPlatformDataDir, _adPlatformDataFilename);
+  utils.createDirectoriesForAssetsSync(_storageLocalAdPlatformDataDir);
+  fs.writeFile(fileFullPath,
+    JSON.stringify(adPlatformData),
+    { flag: 'w+' },
+    function (err) {
+      if (err) {
+        return logger.error(`Error saving adPlatformData: ${err}`);
+      }
+      logger.info(`adPlatformData file was saved under ${fileFullPath}`);
     });
-}
-
-const getAdPlatformRequestHeaders = function(adPlatformDataLastModified) {
-    // Motive: Since we could have potentially switched doors, the "last modified time" for our
-    // local ad-platform-data file might be related to a different door, and we do not want to
-    // get an empty response based on that. Therfeore, after boot, we will always get adPlatformData.
-    const adPlatformGetHeaders = _firstAdPlatformServiceRun ? { } : {
-        'If-Modified-Since': adPlatformDataLastModified
-    };
-
-    _firstAdPlatformServiceRun = false;
-
-    return adPlatformGetHeaders;
-}
-
-const saveAdPlatformJson = function(adPlatformData) {
-    let fileFullPath = path.join(_storageLocalAdPlatformDataDir, _adPlatformDataFilename);
-    utils.createDirectoriesForAssetsSync(_storageLocalAdPlatformDataDir);
-    fs.writeFile(fileFullPath,
-        JSON.stringify(adPlatformData),
-        { flag: 'w+' },
-        function (err) {
-            if (err) {
-                return logger.error(`Error saving adPlatformData: ${err}`);
-            }
-            logger.info(`adPlatformData file was saved under ${fileFullPath}`);
-        });
-}
+};
 
 const buildAdPlatformGetUrl = async function () {
-    const neid = await httpService.getNEID();
-
-    return `${_adPlatformConfig.adPlatformBaseUrl}${neid}?code=${_adPlatformConfig.adPlatformFunctionCode}`;
-}
+  const neid = await httpService.getNEID();
+  
+  return `${_adPlatformConfig.adPlatformBaseUrl}${neid}?code=${_adPlatformConfig.adPlatformFunctionCode}`;
+};
 
 function createDirectoriesForAssets() {
-    utils.createDirectoriesForAssetsSync(_fullDoorAdDirectoryPath, _middleBannerDirectoryPath,
-        _topBannerDirectoryPath, _nativeAdDirectoryPath, _tagDirectoryPath, _labelDirectoryPath, _spotlightAdDirectoryPath);
+  utils.createDirectoriesForAssetsSync(_fullDoorAdDirectoryPath, _middleBannerDirectoryPath,
+    _topBannerDirectoryPath, _nativeAdDirectoryPath, _tagDirectoryPath, _labelDirectoryPath, _spotlightAdDirectoryPath);
 }
