fd05a03f8ea7dc08443727a797c6349cacd1333c
Abhishek Anand <advenabhishek@gmail.com>
2021-07-23 10:37:30 +0530

    added default labels

--- app/downloadAsset.js
+++ app/downloadAsset.js
@@ -1,54 +1,10 @@
-// 'use strict';
-// const express = require('express');
-// const client = require('prom-client');
-// // const collectDefaultMetrics = client.collectDefaultMetrics;
-
-// // define a custom prefix string for application metrics
-// // collectDefaultMetrics();
-
-// // a custom histogram metric which represents the latency
-// // of each call to our API /api/greeting.
-// const histogram = new client.Histogram({
-//   name: 'my_application:hello_duration',
-//   help: 'Duration of HTTP requests in ms',
-//   labelNames: ['method', 'status_code'],
-//   buckets: [0.1, 5, 15, 50, 100, 500]
-// });
-
-// // create the express application
-// const app = express();
-// const port = 9001;
-// // app.use(bodyParser.json());
-// // app.use(bodyParser.urlencoded({extended: false}));
-
-// // our API
-// app.use('/api/greeting', (request, response) => {
-//   // start the timer for our custom metric - this returns a function
-//   // called later to stop the timer
-//   const end = histogram.startTimer();
-//   const name = request.query.name ? request.query.name : 'World';
-//   response.send({content: `Hello, ${name}!`});
-//   // stop the timer
-//   end({ method: request.method, 'status_code': 200 });
-// });
-
-// // expose our metrics at the default URL for Prometheus
-// app.get('/metrics', (request, response) => {
-// //   response.set('Content-Type', client.register.contentType);
-//   response.send(client.register.metrics());
-// });
-
-// app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
-
-
-
-
 const express = require('express')
 const app = express()
 const client = require('prom-client')
 const collectDefaultMetrics = client.collectDefaultMetrics;
-// const Registry = client.Registry;
-// const register = new Registry();
+collectDefaultMetrics({
+    labels: { deviceID: 'abhian-jetson' },
+});
 
 const metricCallCounter = new client.Counter({
     name: 'metric_call_counter',
@@ -69,7 +25,6 @@ app.get('/metrics', async function (req, res) {
 setInterval(() => {
     internalCounter.inc()
 }, 1000)
-
 
 app.listen(9001);
 
